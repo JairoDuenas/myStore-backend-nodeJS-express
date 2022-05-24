42f14486043faf9d84345cc92c74b8f97c8b8a92
JairoDuenas <jairoduenas.ing@gmail.com>
2022-05-24 10:58:06 -0500

    instalación (faker) y GET:parámetros query

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const faker = require('faker');
 
 const app = express();
 const port = 3000;
@@ -7,8 +8,18 @@ app.get('/', (req, res) => {
   res.send('Hola mi server en express');
 });
 
-app.get('/nueva-ruta', (req, res) => {
-  res.send('Hola, soy una nueva ruta');
+app.get('/products', (req, res) => {
+  const products = [];
+  const { size } = req.query;
+  const limit = size || 10;
+  for (let index = 0; index < limit; index++) {
+    products.push({
+      name: faker.commerce.productName(),
+      price: parseInt(faker.commerce.price(), 10),
+      image: faker.image.imageUrl(),
+    });
+  }
+  res.json(products);
 });
 
 app.get('/products', (req, res) => {
@@ -24,6 +35,11 @@ app.get('/products', (req, res) => {
   ]);
 });
 
+// todo lo específico debe ir antes de lo dinámico
+app.get('/products/filter', (req, res) => {
+  res.send('Soy un filter');
+});
+
 app.get('/products/:id', (req, res) => {
   const { id } = req.params;
   id,
@@ -31,6 +47,18 @@ app.get('/products/:id', (req, res) => {
     name: 'Product 2',
     price: 2000
   });
+});
+
+app.get('/users', (req, res) => {
+  const { limit, offset } = req.query;
+  if (limit && offset) {
+    res.json({
+      limit,
+      offset,
+    });
+  } else {
+    res.send('No hay parámetros');
+  }
 });
 
 app.get('/categories/:categoryId/products/:productId', (req, res) => {
