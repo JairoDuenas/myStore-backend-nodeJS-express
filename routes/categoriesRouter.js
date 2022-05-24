9a1d0a4f252dfd78fd0a1fc0ff0de870081bb959
JairoDuenas <jairoduenas.ing@gmail.com>
2022-05-24 12:26:41 -0500

    [ADD] PUT, PATCH Y DELETE

--- routes/categoriesRouter.js
+++ routes/categoriesRouter.js
@@ -2,9 +2,37 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/categories/:categoryId/products/:productId', (req, res) => {
+router.get('/:categoryId/products/:productId', (req, res) => {
   const { categoryId, productId } = req.params;
   res.json({
+    categoryId,
+    productId,
+  });
+});
+
+router.post('/', (req, res) => {
+  const body = req.body;
+  res.json({
+    message: 'created',
+    data: body
+  });
+});
+
+router.patch('/:categoryId/products/:productId', (req, res) => {
+  const { categoryId, productId } = req.params;
+  const body = req.body;
+  res.json({
+    message: 'update',
+    data: body,
+    categoryId,
+    productId,
+  });
+});
+
+router.delete('/:categoryId/products/:productId', (req, res) => {
+  const { categoryId, productId } = req.params;
+  res.json({
+    message: 'deleted',
     categoryId,
     productId,
   });
